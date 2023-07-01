c0a3e6c75fd936cda2cb389ecc9ae14fd19f3f81
Toluwani <oluwoletoluwani@gmail.com>
2023-06-30 19:06:04 -0500

    edited allowed hosts

--- asopo/settings.py
+++ asopo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.heroku.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
