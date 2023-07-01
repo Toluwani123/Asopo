78433191a0c0e7385aee90fd676d232beb50b26c
Toluwani <oluwoletoluwani@gmail.com>
2023-07-01 09:39:59 -0500

    configuration for https websocket

--- main/routing.py
+++ main/routing.py
@@ -3,5 +3,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/creator_chat/(?P<booth_name>\w+)/$', consumers.BoothConsumer.as_asgi()),
+    re_path(r'wss/creator_chat/(?P<booth_name>\w+)/$', consumers.BoothConsumer.as_asgi()),
 ]
